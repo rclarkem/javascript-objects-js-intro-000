4778d392b98ae4f6b42dc3a3df6f2e2a7da7941f
cheerful-animator-9488 <r.clarkemitchell@gmail.com>
2019-04-15 02:01:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ let playlist = {
 };
 
 function  updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({},playlist, artistName[songTitle])
+  return Object.assign(playlist, artistName[songTitle])
 }
